2a20c8ccc8e0e4211581f7384b24b7aee6dc30a2
JaemoKoo <devgoozz@gmail.com>
2023-02-09 22:18:35 +0900

    fix: 필요없는 코드 삭제

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
-const serviceAccout = require("../ssr1-admin.json");
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req: Request, res: Response) => {
@@ -11,5 +10,4 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(8000, () => {
   console.log("Server Start");
-  console.log(serviceAccout);
 });
