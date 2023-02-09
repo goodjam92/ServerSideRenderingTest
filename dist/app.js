46cd9b7196024dee1000087d01ea815521d7d0bd
JaemoKoo <devgoozz@gmail.com>
2023-02-09 22:18:17 +0900

    chore: firebase와 express 연동 작업

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,31 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
+
+var adimn = require("firebase-admin");
+var firestore = require("firebase-admin/firestore");
+
+var serviceAccount = require("../ssr1-admin.json");
+adimn.initializeApp({
+  credential: adimn.credential.cert(serviceAccount),
+});
+
+const dbService = firestore.getFirestore();
+if (!dbService) {
+  alert("db 초기화 실패");
+}
+
 app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
 app.get("/", function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, "../public/index.html"));
+  res.sendFile(path_1.default.join(__dirname, "../public/index.html"));
 });
-app.listen("8001", function () {
-    console.log("Server Start");
+app.listen("8000", function () {
+  console.log("Server Start");
 });
--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
+const serviceAccout = require("../ssr1-admin.json");
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.listen("8001", () => {
+app.listen(8000, () => {
   console.log("Server Start");
+  console.log(serviceAccout);
 });
