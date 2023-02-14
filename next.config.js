85ae2f4d6efb67d42e8871a326f152bcef401d9c
JaemoKoo <devgoozz@gmail.com>
2023-02-14 14:06:54 +0900

    fix: 404에러가 나서 firebase 잔재 삭제

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  basePath: "/{레포지토리 이름}",
-}
-
 module.exports = {
   experimental: {
     appDir: true,
   },
-  basePath: "/{레포지토리 이름}",
   images: {
     remotePatterns: [
       {
