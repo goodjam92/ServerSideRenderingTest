aea6f6d61f794dc1772d069aa7abd0d8b71d1d16
JaemoKoo <devgoozz@gmail.com>
2023-02-15 10:17:37 +0900

    fix: 테스트를 위한 이미지 파일 추가 및 설정 변경

--- pages/serversiderendering/index.tsx
+++ pages/serversiderendering/index.tsx
@@ -30,18 +30,18 @@ export const getStaticProps: GetStaticProps<{
   images: string[];
 }> = async (context) => {
   const images: string[] = [
-    "https://upload.wikimedia.org/wikipedia/commons/8/89/Aerial_view_of_National_Museum_of_American_History.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/2/23/%281%29Moonrise_Darling_Harbour.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/8/88/12-07-13-washington-by-RalfR-10.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/6/64/%27Witch_Head%27_Brews_Baby_Stars_%2810592267924%29.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/2/2b/%27Zur_steinernen_Glocke_%27_Altst%C3%A4dter_Ring.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/7/71/%281%29Campbells_Stores.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/f/f6/%281%29Beauchamp_Falls_Blue_Mountains.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/8/8d/%281%29Bare_Island.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/f/f6/%281%29Darling_Harbour_concert.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/b/b5/%281%29Figtree_House_Hunters_Hill-1.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/e/e9/%281%29Sydney_Opera_House.jpg?uselang=ko",
-    "https://upload.wikimedia.org/wikipedia/commons/7/7c/%281%29Vailele_Hunters_Hill.jpg?uselang=ko",
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpg",
+    "4.jpeg",
+    "5.jpeg",
+    "6.jpeg",
+    "7.jpeg",
+    "8.jpeg",
+    "9.jpeg",
+    "10.jpeg",
+    "11.jpeg",
+    "12.jpeg",
   ];
 
   return { props: { images } };
